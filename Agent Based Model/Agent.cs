94706353b3a0cd0acfc4bb54552855b6f780aedb
sanzh_m <sanzh98@gmail.com>
2017-11-01 14:40:37 +0800

    First commit

---
+++ Agent Based Model/Agent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent_Based_Model
+{
+    class Agent
+    {
+        ConFunc ConFunc;
+        ProFunc ProFunc;
+        Prod Prod;
+        public Agent(int num)
+        {
+            //ConFunc, ProFunc and Prod assignment
+        }
+    }
+}
---
+++ Agent Based Model/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agent_Based_Model
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+    public enum ConFunc { }
+    public enum ProFunc { }
+    public enum Prod { }
+}
